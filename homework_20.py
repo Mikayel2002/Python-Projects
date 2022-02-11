96365093ae581f8eae88aafc8317b75219358647
Mikayel2002 <97906185+Mikayel2002@users.noreply.github.com>
2022-02-12 00:15:03 +0400

    Add files via upload

---
+++ homework_20.py
@@ -0,0 +1,31 @@
+import requests
+import threading
+import json
+
+
+with open("images.json") as json_file:
+    data = json.load(json_file)
+
+print(data)
+
+
+def download_images(name, url):
+    with open("{}.jpg".format(name), "wb") as image:
+        try:
+            response = requests.get(url)
+        except Exception as err:
+            print("Something happened {}".format(err))
+
+        if response.status_code == 200:
+            image.write(response.content)
+            print("Image {} is downloaded".format(name))
+
+
+thread_list = []
+
+for item in data:
+    t = threading.Thread(target=download_images, args=(item["name"], item["url"]))
+    thread_list.append(t)
+
+for thread in thread_list:
+    thread.start()
